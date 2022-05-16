05c9336effd6c8e32aa7b5cfe81b7224b26be283
BieleczkiGergo <bgergo8b@gmail.com>
2022-05-16 21:02:40 +0200

    Worked some on the plan

---
+++ README.md
@@ -0,0 +1,4 @@
+This is an algebra library, so to say. It will help working with algebraic expressions.
+The purpouse of the plan directory would be to visualize the goal using python.
+Also, the plan files will be full of explanation in the form of comments.
+For now, I don't even have an idea of how the higher level things are going to work.
--- plan/methods.py
+++ plan/methods.py
@@ -1,2 +1,41 @@
-
+#/bin/python3
 #This file is for defining the methods and classes
+
+ADDITION = True
+MULTIPLICATION = False
+
+#mElement (math element... how creative...) is the main element we will be working with
+class mElement:
+    #The most important variable is the base, which is, hovewer surprising, the base
+    #of the mElement. It can either store an integer, a character, or a list of mElements.
+    #(Only having one mElement would serve little purpouse)
+    #In the case that it holds an mElement, the type variable comes into play. It will
+    #determine if the mElement is an addition or a multiplication
+    __slots__ = ("exp", "den", "base", "type", "expType", "denType", "baseType")
+
+    def __init__(self, base, type=ADDITION, exp=1, den=1):
+        self.base = base
+        self.type = type
+        self.exp = exp
+        self.den = den
+        self.baseType = type(self.base)
+        
+
+    def __eq__(self, other):
+        #if self.base == other.base and self.den == other.den and self.exp == other.exp and self.type == other.type:
+        #   return True
+        if self.type == other.type:
+            if self.base == other.base:
+                if self.exp == other.exp:
+                    if self.den == other.den:
+                        return True
+
+
+        return False
+
+
+element1 = mElement(4)
+element2 = mElement(3)
+
+print(element1 == element2)
+print(type(element1))
